12b6bd9a583cf1bb439af0d6ff50832422be8a6e
Mayank Makwana <mayankmmmx@gmail.com>
2017-01-06 21:24:40 -0500

    fixed script

--- api/create_db_script.py
+++ api/create_db_script.py
@@ -1,6 +1,6 @@
 import peewee as pw
 from Routes.db import RegistrationForm
-from Routes.constants
+from Routes import constants
 
 db = peewee.MySQLDatabase(constants.DB_NAME, user=constants.DB_USER_NAME,
                             passwd=constants.DB_PASSWORD)
